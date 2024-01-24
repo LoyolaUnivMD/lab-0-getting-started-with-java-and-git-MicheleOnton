2807efd48001871c77eeee3b01e1cae0606dda96
Michele Onton <maontoncueva@loyola.edu>
2024-01-23 22:51:05 -0500

    in class activity

--- Main.java
+++ Main.java
@@ -1,41 +1,36 @@
-
-// members : Michele Onton & Zhain Skovron
+// Full Name : Michele Onton Cueva
 
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // Ask the user for seconds between births
-        System.out.println("How many seconds between births? ");
-        int births_per_second = input.nextInt();
-        // Ask the user the death rate in seconds
-        System.out.println("How many second between death? ");
-        int deaths_per_second = input.nextInt();
-        // Ask the user the rate of new immigrants in seconds
-        System.out.println("How many seconds between immigrations? ");
-        int immigrants_per_second = input.nextInt();
-        // Ask the user for the current population
-        System.out.println("What is the current population? ");
-        int current_population = input.nextInt();
-        // Ask the user how many years into the future the user wants to know the population
-        System.out.println("How many years in the future? ");
-        int num_years = input.nextInt();
 
-        // Find how many seconds are in one year
-        final int seconds_in_year = 360 * 24 * 60 * 60 ;
+        // Ask the user for his weight in lbs
+        System.out.println("What is your weight in lbs? ");
+        double weight = input.nextDouble();
+        // Ask the user for his height in ft
+        System.out.println("What is your height in ft? ");
+        double height = input.nextDouble();
 
-        // Calculate births, deaths, and immigrants per year
-        int births_per_year = seconds_in_year / births_per_second ;
-        int deaths_per_year = seconds_in_year / deaths_per_second ;
-        int immigrants_per_year = seconds_in_year / immigrants_per_second ;
+        // Convert lbs to kg
+        double weight_kg = weight * 0.45;
 
-        // Calculate the future population
-        int future_population = current_population + (num_years * (immigrants_per_year + births_per_year - deaths_per_year));
+        // Convert ft to m
+        double height_m = height * 0.3048;
 
-        // Print out final statements
-        System.out.println("The US population in " + num_years + " years will be " + future_population);
-        System.out.println("The population increased");
+        // Calculate Health Metrics Index (HMI)
+        double hmi_value = weight_kg / Math.pow(height_m,2);
 
+        // Print out final decisions
 
+        if (hmi_value < 18.5) {
+            System.out.println("HMI of " + hmi_value + " needs calorie increase.");
+        } else if (hmi_value >= 18.5 && hmi_value < 25) {
+            System.out.println("HMI of " + hmi_value + " should maintain current calorie intake.");
+        } else if (hmi_value >= 25 && hmi_value < 30 ) {
+            System.out.println("HMI of " + hmi_value + " should reduce calorie intake.");
+        } else {
+            System.out.println("HMI of " + hmi_value + " needs calorie deficit.");
+        }
     }
 }
